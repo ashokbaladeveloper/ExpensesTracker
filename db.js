cada88a9a1ce0e9db919aa7ddbb563ca203c35a9
ashokbaladeveloper <ashokbaladeveloper@gmail.com>
2026-01-12 18:18:25 +0530

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,15 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err);
+    // process.exit(-1); // Optionally exit if you want to let PM2/Nodemon restart
+});
 module.exports = pool;
+
